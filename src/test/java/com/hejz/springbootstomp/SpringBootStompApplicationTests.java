c783035163fa38bd2bac5d5d2d9737fd0489275e
hejz <75412985@qq.com>
2021-11-30 18:04:46 +0800

    初始化

---
+++ src/test/java/com/hejz/springbootstomp/SpringBootStompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hejz.springbootstomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStompApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
